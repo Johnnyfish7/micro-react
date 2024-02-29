424f37cf0745ba5b1ef465252462013992491df7
吴伟强 <weiqiang.wu@zhangmen.com>
2024-02-29 16:59:46 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// concurrent mode
+// 并行 同时进行的
+
+import { createElement, render } from './micro-react'
+
+const element = createElement(
+  'h1',
+  { id: 'title' },
+  'Hello React',
+  createElement('a', { href: 'https://bilibili.com' }, 'Click Me!'),
+)
+
+const container = document.querySelector('#root')
+
+render(element, container)
+
+console.log(element)
